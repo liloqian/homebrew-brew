1af7fa6ad5ec316bd7c10d57ecea24730e08229d
qian <1768031241@qq.com>
2023-04-08 20:56:58 +0800

    addd formula

---
+++ Formula/hello.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Hello < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/liloqian/homebrew-brew/raw/main/helloBrew_0.0.1.tar.gz"
+  sha256 "8e5a9d1383725b2d0657cfa4a2554a083721760b1dd0195c608cc15df05f886b"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    #system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "helloBrew" 	
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test hello_0.0.1`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
